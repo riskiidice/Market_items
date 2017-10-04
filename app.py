663c6fd22ddefbe245478adfd478ad7ee6f28334
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2017-10-04 22:38:56 +0700

    Fixed import db

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
+
 
 from security import authenticate,identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store,StoreList
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "Tikumporn"
 api = Api(app)
-
-@app.before_first_request
-def create_table():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity) #/auth
 
